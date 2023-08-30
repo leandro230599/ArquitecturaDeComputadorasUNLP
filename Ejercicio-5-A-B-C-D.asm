40ebe715e822acae20fa301a3b14f5bc5c626eff
Leandro López <51353353+leandro230599@users.noreply.github.com>
2023-08-30 01:12:24 -0300

    Update and rename Ejercicio-5-A.asm to Ejercicio-5-A-B-C-D.asm
    Añadido B,C y D

---
+++ Ejercicio-5-A-B-C-D.asm
@@ -0,0 +1,66 @@
+A)
+
+          ORG 1000h
+            A dw 5h
+            B dw 6h
+            C dw 2h
+            D dw ?
+          
+          org 2000h
+            mov ax, A
+            add ax, B
+            sub ax, C
+            mov bx, offset D
+            mov [bx], ax
+            hlt
+          end
+
+------------------------------------------------------
+B)
+        ORG 1000h
+          A dw 5h
+          B dw 6h
+          C dw 2h
+          D dw ?
+        
+        ORG 2000h
+          call calculo
+          hlt
+        
+        ORG 3000h
+        calculo:   mov ax, A
+                   add ax, B
+                   sub ax, C
+                   mov bx, offset D
+                   mov [bx], ax
+                   ret
+        end
+
+------------------------------------------------------
+C)
+
+        ORG 1000h
+          A dw 5h
+          B dw 6h
+          C dw 2h
+          D dw ?
+        
+        ORG 2000h
+          mov ax, A
+          mov bx, B
+          mov cx, C
+          call calculo
+          mov bx, offset D
+          mov [bx], dx
+          hlt
+        
+        ORG 3000h
+        calculo:   add ax, bx
+                   sub ax, cx
+                   mov dx, ax
+                   ret
+        end
+
+------------------------------------------------------
+D)
+ Las del inciso B no se podrian reutilizar dado a que la carga se hace en la rutina, y el C si se puede reutilizar porque la carga se hace en el programa principal y le pasa el argumento de lo que tiene que utilizar
