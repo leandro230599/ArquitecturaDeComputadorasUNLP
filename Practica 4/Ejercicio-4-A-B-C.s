869c6d362e8df480d4f93d2516d92a6cfa17b50b
Leandro López <51353353+leandro230599@users.noreply.github.com>
2023-11-06 18:10:30 -0300

    Update Ejercicio-4-A-B-C.s

--- Practica 4/Ejercicio-4-A-B-C.s
+++ Practica 4/Ejercicio-4-A-B-C.s
@@ -24,6 +24,19 @@ a) Ejecutar en simulador con Forwarding habilitado. ¿Qué tarea realiza? ¿Cuá
 
 b) Re-Ejecutar el programa con la opción Configure/Enable Branch Target Buffer habilitada. Explicar la ventaja de usar este
 método y cómo trabaja.
-      
+      La ventaja de BTB es que predice si habra salto o no, si hay salto, predice la direccion de salto, si salto o no
+      la direccion de la instruccion de salto leida, esto sirve porque si tengo un loop condicional ponele de leer el 
+      registro R1, que este tiene el valor 10 y se va decrementando y si no llega a 0, salte, el BTB me permite predecir que
+      va a saltar y el primer caso y el ultimo fallaran leyendo la siguiente instruccion del salto y quitandose del cause,
+      las demas veces predecira correctamente, en el ultimo caso, predecira que debe saltar, pero al llegar a la etapa ID
+      vera que no debia saltar, por lo que la siguiente instruccion que estaba en IF, se descartara y tomara la que debia
+      ejecutar
 
 c) Confeccionar una tabla que compare número de ciclos, CPI, RAWs y Branch Taken Stalls para los dos casos anteriores.
+SIN BTB: 
+Ciclos: 71  Instrucciones: 43  CPI: 1,651  RAWs: 16  BTS: 8
+
+CON BTB: 
+Ciclos: 67  Instrucciones: 43  CPI: 1,558  RAWs: 16  BTS: 4
+
+
