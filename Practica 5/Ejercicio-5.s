1854075dd8039c7877207ac59ebaef59e05081e7
Leandro López <51353353+leandro230599@users.noreply.github.com>
2023-11-08 01:06:41 -0300

    Update Ejercicio-5.s

--- Practica 5/Ejercicio-5.s
+++ Practica 5/Ejercicio-5.s
@@ -9,11 +9,17 @@
                   r1          $at               Reservado para uso del ensamblador
                   r2-r3       $v0-$v1           Valor retornado por subrutina
                   r4-r7       $a0-$a3           Argumentos para una subrutina
-                  r8-r15      $t0-$t7           Temporarios para subrutina
-                  r16-r23     $s0-$s7           Variables de subrutinas, preservar sus valores en llamada                      SI
-                  r24-r25     $t8-$t9           Temporarios para subrutina
+                  r8-r15      $t0-$t7           Temporarios para subrutina, no son conservados en llamado
+                  r16-r23     $s0-$s7           Variables de subrutinas, salvar sus valores en llamada                      SI
+                  r24-r25     $t8-$t9           Temporarios para subrutina, no son conservados en llamado
                   r26-r27     $k0-$k1           Usados por manejador de interrupciones/trap
-                  R28         $gp               Puntero global (acceso a var static/extern)
-                  R29         $sp               Puntero de pila
-                  R30         $fp               Frame pointer, puntero de cuadro
-                  R31         $ra               Direccion de retorno de subrutina
+                  R28         $gp               Global pointer, puntero global (acceso a var static/extern)                 SI
+                  R29         $sp               Stack pointer, puntero de tope de pila                                      SI
+                  R30         $fp               Frame pointer, puntero al marco actual de la pila                           SI
+                  R31         $ra               Return address, direccion de retorno de subrutina                           SI
+
+
+
+    Complete la tabla anterior explicando el uso que normalmente se le da cada uno de los registros nombrados. Marque
+    en la columna “¿Preservado?” si el valor de cada grupo de registros debe ser preservado luego de realizada una
+    llamada a una subrutina. Puede encontrar información útil en el apunte “Programando sobre MIPS64”. 
