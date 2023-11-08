2178911481df3969f1d4963dabcdfdd65eee98dc
Leandro López <51353353+leandro230599@users.noreply.github.com>
2023-11-07 21:10:59 -0300

    Create Ejercicio-2.s

---
+++ Practica 5/Ejercicio-2.s
@@ -0,0 +1,7 @@
+2)  Es posible convertir valores enteros almacenados en alguno de los registros r1-r31 a su representación equivalente
+    en punto flotante y viceversa. Describa la funcionalidad de las instrucciones mtc1, cvt.l.d, cvt.d.l y mfc1. 
+
+MTC1= Copia 64 bits de un registro entero, a un registro de punto flotante
+CVT.L.D= Convierte a entero un valor de un registro de punto flotante, dejandolo en otro registro de punto flotante
+CVT.D.L= Converte a punto flotante un valor de un registro de punto flotante, dejandolo en otro registro de punto flotante
+MFC1= Copia 64 bits de un registro de punto flotante, a un registro entero
